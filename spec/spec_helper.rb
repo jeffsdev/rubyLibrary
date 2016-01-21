dfac6548867f86b8eba7a2d53b295fb402ea13f5
Gregory K and Jeff Seymour <gregkanteres@gmail.com>
2016-01-21 14:32:43 -0800

    Add styling

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,5 +10,6 @@ RSpec.configure do |config|
   config.after(:each) do
     DB.exec("DELETE FROM patrons *;")
     DB.exec("DELETE FROM books *;")
+    DB.exec("DELETE FROM patrons_books *;")
   end
 end
